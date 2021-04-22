1616890a7ea57a8cd42c49a3bb2f9f4b4c5ec7b6
tejas181995 <tejasdev95@gmail.com>
2021-04-22 17:05:47 +0530

    ADD. GET request method to pass query parameter as name

--- src/main/java/com/bridelabz/demo/controller/HelloMsgResController.java
+++ src/main/java/com/bridelabz/demo/controller/HelloMsgResController.java
@@ -1,6 +1,8 @@
 package com.bridelabz.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,9 @@ public class HelloMsgResController {
 	@RequestMapping(value = {"", "/", "/home"})
 	public String sayHello() {
 		return "hello from bridgelabz";
+	}
+	@RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+	public String sayHello(@RequestParam(value = "name")  String name) {
+		return "Hello" +" " + name +"!";
 	}
 }
