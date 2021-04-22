6285746ebb345e6063bc743c3317fcfb22886c00
tejas181995 <tejasdev95@gmail.com>
2021-04-22 17:57:31 +0530

    ADD. used post request method and passed first name and last name to body

--- src/main/java/com/bridelabz/demo/controller/HelloMsgResController.java
+++ src/main/java/com/bridelabz/demo/controller/HelloMsgResController.java
@@ -2,10 +2,14 @@ package com.bridelabz.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bridelabz.demo.model.User;
 
 @RestController
 @RequestMapping("/hello")
@@ -22,5 +26,9 @@ public class HelloMsgResController {
 	@GetMapping("/param/{name}")
 	public String sayHelloParam(@PathVariable String name) {
 		return "Hello" + " " + name + " ..!!";
+	}
+	@PostMapping("/post")
+	public String sayHello(@RequestBody User user) {
+		return "Hello"+  " " + user.getFirstName() + " " + user.getLastName() + " " + "..!";
 	}
 }
