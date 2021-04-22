bae2692b023136ba28253acdcfdc825c48597ccd
tejas181995 <tejasdev95@gmail.com>
2021-04-22 16:29:38 +0530

    ADD. hello message to localhost

---
+++ src/main/java/com/bridelabz/demo/HelloMsgAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridelabz.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMsgAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloMsgAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridelabz/demo/HelloMsgAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridelabz.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMsgAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
